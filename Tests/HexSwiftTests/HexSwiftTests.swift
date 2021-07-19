25f8922e59aa6bda37da15df5e3c2aa99e9d481e
Stan Rosenbaum <ForeverTangent@gmail.com>
2021-07-19 14:55:39 -0400

    First Unit Test

--- Tests/HexSwiftTests/HexSwiftTests.swift
+++ Tests/HexSwiftTests/HexSwiftTests.swift
@@ -2,10 +2,13 @@
     @testable import HexSwift
 
     final class HexSwiftTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(HexSwift().text, "Hello, World!")
-        }
+		func testHex() {
+			// This is an example of a functional test case.
+			// Use XCTAssert and related functions to verify your tests produce the correct
+			// results.
+
+			let hex1: Hex<Int32>? = Hex<Int32>(q: 1, r: 1, s: -2)
+			XCTAssert(hex1 != nil)
+		}
+
     }
