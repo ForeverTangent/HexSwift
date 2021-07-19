fec4448511a1985e6fd400bfe42907c18955f42b
Stan Rosenbaum <ForeverTangent@gmail.com>
2021-07-19 17:16:16 -0400

    Now with Addition, Subtraction and Multiplication ops.

--- Sources/HexSwift/Hex.swift
+++ Sources/HexSwift/Hex.swift
@@ -10,19 +10,19 @@ import Foundation
 /**
 Cube storage, cube constructor
 */
-struct Hex<E: SignedInteger & SIMDScalar> {
+struct Hex<E: SignedInteger & SIMDScalar>: Codable {
 
-	// MARK: Properties
+	// MARK: - Properties
 
 	let data: SIMD3<E>
 	var q: E { data.x }
 	var r: E { data.y }
 	var s: E { data.z }
 
-	// MARK: Init
+	// MARK: - Inits
 
 	/**
-	Hex coordinates q + r + s must equal == 0
+	Hex cube coordinates q + r + s must equal == 0
 	*/
 	init?(q: E, r: E, s: E) {
 		guard
@@ -33,5 +33,53 @@ struct Hex<E: SignedInteger & SIMDScalar> {
 		self.data = SIMD3<E>(x: q, y: r, z: s)
 	}
 
+	/**
+	Hex axial coordinates
+	*/
+	init?(q: E, r: E) {
+		self.data = SIMD3<E>(x: q, y: r, z: -q - r)
+	}
+
+}
+
+
+extension Hex {
+	static func + (lhs: Hex<E>, rhs: Hex<E>) -> Hex<E> {
+		return Hex<E>(q: lhs.q + rhs.q, r: lhs.r + rhs.r, s: lhs.s + rhs.s)!
+	}
+
+	static func - (lhs: Hex<E>, rhs: Hex<E>) -> Hex<E> {
+		return Hex<E>(q: lhs.q - rhs.q, r: lhs.r - rhs.r, s: lhs.s - rhs.s)!
+	}
+
+	static func += (lhs: inout Hex<E>, rhs: Hex<E>) -> Hex<E> {
+		return lhs + rhs
+	}
+
+	static func -= (lhs: inout Hex<E>, rhs: Hex<E>) -> Hex<E> {
+		return lhs - rhs
+	}
+
+	static func * (lhs: Hex<E>, rhs: Int) -> Hex<E> {
+		return Hex(q: lhs.q * E(rhs), r: lhs.r * E(rhs), s: lhs.s * E(rhs))!
+	}
+
+	static func * (lhs: Int, rhs: Hex<E>) -> Hex<E> {
+		return rhs * lhs
+	}
+
+}
+
+
+/**
+Add in Equatable Protocol
+*/
+extension Hex: Equatable { }
+
+
+extension Hex: CustomStringConvertible {
+	var description: String {
+		return "HEX q:\(self.q), r:\(self.r), s:\(self.s)"
+	}
 
 }
--- Tests/HexSwiftTests/HexSwiftTests.swift
+++ Tests/HexSwiftTests/HexSwiftTests.swift
@@ -20,4 +20,51 @@
 					  "Hex coordinates q: \(String(describing: testHex?.q)) + r: \(String(describing: testHex?.r)) + s: \(String(describing: testHex?.s)) must equal == 0")
 		}
 
+
+		func testHexAddition() {
+			// This is an example of a functional test case.
+			// Use XCTAssert and related functions to verify your tests produce the correct
+			// results.
+
+			let hex1 = Hex<Int32>(q: 1, r: 1, s: -2)!
+			let hex2 = Hex<Int32>(q: 1, r: 1, s: -2)!
+
+			let hex3 = hex1 + hex2
+			print(hex3)
+			let targetHex = Hex<Int32>(q: 2, r: 2, s: -4)
+			XCTAssert(hex3 == targetHex, "\(hex3) == \(String(describing: targetHex))")
+		}
+
+		func testHexSubstraction() {
+			// This is an example of a functional test case.
+			// Use XCTAssert and related functions to verify your tests produce the correct
+			// results.
+
+			let hex1 = Hex<Int32>(q: 1, r: 1, s: -2)!
+			let hex2 = Hex<Int32>(q: 1, r: 1, s: -2)!
+
+			let hex3 = hex1 - hex2
+			print(hex3)
+			let targetHex = Hex<Int32>(q: 0, r: 0, s: 0)
+			XCTAssert(hex3 == targetHex, "\(hex3) == \(String(describing: targetHex))")
+		}
+
+		func testHexMultiplication() {
+			// This is an example of a functional test case.
+			// Use XCTAssert and related functions to verify your tests produce the correct
+			// results.
+
+			let hex1 = Hex<Int32>(q: 1, r: 1, s: -2)!
+
+			let hex2 = hex1 * 3
+			print(hex2)
+			let targetHex1 = Hex<Int32>(q: 3, r: 3, s: -6)
+			XCTAssert(hex2 == targetHex1, "\(hex2) == \(String(describing: targetHex1))")
+
+			let hex3 = 3 * hex1
+			print(hex3)
+			let targetHex2 = Hex<Int32>(q: 3, r: 3, s: -6)
+			XCTAssert(hex3 == targetHex2, "\(hex3) == \(String(describing: targetHex2))")
+		}
+
     }
